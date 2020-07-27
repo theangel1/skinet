daa1a344d464d030f0e6ab2930472ace2f252064
Angel Omar Pinilla <angel.pinilla@outlook.es>
2020-07-27 15:51:40 -0400

    Initial Commit

---
+++ Infraestructure/Data/StoreContext.cs
@@ -0,0 +1,15 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infraestructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products {get;set;}
+        
+    }
+}
