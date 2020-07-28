8a07022cf75bc0e847a836e454e108804d75c976
Angel Omar Pinilla <angel.pinilla@outlook.es>
2020-07-28 14:16:50 -0400

    End of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,5 @@
+using API.Helpers;
+using AutoMapper;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
@@ -23,6 +25,9 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped(typeof(IGenericRepository<>) , (typeof(GenericRepository<>)));
+            services.AddAutoMapper(typeof(MappingProfiles));
+
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => 
             x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
@@ -39,6 +44,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            
+            app.UseStaticFiles();
 
             app.UseAuthorization();
 
--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
@@ -26,9 +27,14 @@ namespace Infrastructure.Data
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
-            return await _db.Products.
-            Include(p => p.ProductType)
-            .Include(p => p.ProductBrand).ToListAsync();
+            var typeId = 1;
+
+            var products = _db.Products.Where(x => x.ProductTypeId == typeId).Include(p => p.ProductType).ToListAsync();
+            
+            return await _db.Products
+            .Include(p => p.ProductType)
+            .Include(p => p.ProductBrand)
+            .ToListAsync();
         }
         public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
         {
