d9e6722ed783e8574a8c4f8e298e0b92aefafef4
Angel Omar Pinilla <angel.pinilla@outlook.es>
2020-07-28 17:40:10 -0400

    End of section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,6 +33,13 @@ namespace API
 
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
 
 
         }
@@ -49,6 +56,8 @@ namespace API
             app.UseRouting();
 
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
