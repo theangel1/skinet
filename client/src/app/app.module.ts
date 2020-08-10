610ac9c16fc60d00b8b157b0dc2daede23c07048
Angel Omar Pinilla <angel.pinilla@outlook.es>
2020-08-10 12:03:05 -0400

    End of section 12

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +8,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {CoreModule} from './core/core.module';
 import {ShopModule} from './shop/shop.module';
 import { HomeModule } from './home/home.module';
+import {ErrorInterceptor} from './core/interceptors/error.interceptor';
+import {LoadingInterceptor} from './core/interceptors/loading.interceptors';
+import {NgxSpinnerModule} from 'ngx-spinner';
 
 @NgModule({
   declarations: [
@@ -20,9 +23,14 @@ import { HomeModule } from './home/home.module';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    HomeModule
+    HomeModule,
+    NgxSpinnerModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true}
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -11,8 +11,8 @@ import { ShopParams } from '../shared/models/shopParams';
   styleUrls: ['./shop.component.scss']
 })
 
-export class ShopComponent implements OnInit {
-@ViewChild('search', {static: true}) searchTerm: ElementRef;
+export class ShopComponent implements OnInit {  
+@ViewChild('search', {static: false}) searchTerm: ElementRef;
 products: IProduct[];
 brands: IBrand[];
 types: IType[];
