e30ad478641fec6337988e1073f03eb777db5634
Angel Omar Pinilla <angel.pinilla@outlook.es>
2020-08-24 18:04:44 -0400

    end of section 17

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BasketService } from './basket/basket.service';
+import { AccountService } from './account/account.service';
 
 
 @Component({
@@ -10,17 +11,31 @@ import { BasketService } from './basket/basket.service';
 export class AppComponent implements OnInit{
   title = 'Skinet';
   
-  constructor(private basketService: BasketService){}
+  constructor(private basketService: BasketService,
+    private accountService: AccountService){}
 
   ngOnInit() {
+   this.loadBasket();
+   this.loadCurrentUser();
+  }
+
+  loadCurrentUser(){
+    const token = localStorage.getItem('token');
+    this.accountService.loadCurrentUser(token).subscribe(() => {
+        console.log('loaded user');
+      }, error => {
+        console.log(error);
+      });
+  }
+
+  loadBasket(){
     const basketId = localStorage.getItem('basket_id');
     if (basketId){
-      this.basketService.getBasket(basketId).subscribe(()=>{
+      this.basketService.getBasket(basketId).subscribe(() => {
         console.log('initialised basket');
       }, error => {
         console.log(error);
-      })
+      });
     }
-
   }
 }
--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -8,6 +8,7 @@ import { ServerErrorComponent } from './server-error/server-error.component';
 import {BreadcrumbModule} from 'xng-breadcrumb';
 import {ToastrModule} from 'ngx-toastr';
 import { SectionHeaderComponent } from './section-header/section-header.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -18,6 +19,7 @@ import { SectionHeaderComponent } from './section-header/section-header.componen
     CommonModule,
     RouterModule,
     BreadcrumbModule,
+    SharedModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
       preventDuplicates: true
